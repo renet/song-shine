97b32cee0ba43e0e4d6c6ed94549bcea9880c5c7
René Schubert <reneschubert@gmx.de>
2019-03-26 00:47:44 +0100

    add base styles and material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
-export default () => <div>Welcome to Next.js!</div>;
+import { createGlobalStyle } from 'styled-components';
+import Head from 'next/head';
+import Button from '@material-ui/core/Button';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500');
+    @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  }
+`
+
+const App = () => (
+  <div>
+    <Button variant="contained" color="primary">
+      Welcome to Next.js!
+    </Button>
+    <GlobalStyles />
+  </div>
+);
+
+export default App;
