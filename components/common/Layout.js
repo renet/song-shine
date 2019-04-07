bdabfea1a32237b387daf630cb48dee3e90f3071
René Schubert <reneschubert@gmx.de>
2019-04-07 16:22:19 +0200

    Add add icon to Layout

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,36 +1,39 @@
-import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 import Header from "./Header";
 import SideMenu from "./SideMenu";
-
-const toggleTheme = (setTheme, currentTheme) =>
-  setTheme(currentTheme === "dark" ? "light" : "dark");
 
 const styles = theme => ({
   root: {
     flexGrow: 1
   },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: "center",
-    color: theme.palette.text.secondary
+  fab: {
+    bottom: theme.spacing.unit * 2,
+    position: "absolute",
+    right: theme.spacing.unit * 2
   }
 });
 
+/** Basic layout wrapping every content */
 const Layout = ({ children, classes }) => (
   <div className={classes.root}>
-    <Grid container spacing={24}>
+    <Grid container spacing={24} justify="center">
       <Header />
       <SideMenu />
       {children}
+      <Fab className={classes.fab} color="primary">
+        <AddIcon />
+      </Fab>
     </Grid>
   </div>
 );
 
 Layout.propTypes = {
-  classes: PropTypes.object.isRequired
+  /** Page content */
+  children: PropTypes.node
 };
 
 export default withStyles(styles)(Layout);
