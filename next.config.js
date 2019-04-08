3b83f9aeb493d210d1227b18fe7366e540d74d1b
René Schubert <reneschubert@gmx.de>
2019-04-09 00:05:02 +0200

    Add basic auth to server-side fetch

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const HOSTNAME = process.env.HOSTNAME || "localhost";
 const PORT_PUBLIC = process.env.PUBLIC_PORT;
 const PORT_SERVER = process.env.PORT || 3000;
 const PROTOCOL = process.env.PROTOCOL || "http";
+const user = process.env.AUTH_USER;
+const password = process.env.AUTH_PASSWORD;
 
 module.exports = {
   publicRuntimeConfig: {
@@ -10,6 +12,8 @@ module.exports = {
     }/api/db`
   },
   serverRuntimeConfig: {
-    API_URL: `http://localhost${PORT_SERVER ? `:${PORT_SERVER}` : ""}/api/db`
+    API_URL: `http://localhost${PORT_SERVER ? `:${PORT_SERVER}` : ""}/api/db`,
+    user,
+    password
   }
 };
