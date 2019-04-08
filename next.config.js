fdff27fea41576591abfe3ecf11e6aaadebfc0fd
René Schubert <reneschubert@gmx.de>
2019-04-08 20:54:39 +0200

    Fix port config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const HOSTNAME = process.env.HOSTNAME || "localhost";
 const PORT = process.env.PORT || 3000;
 const PROTOCOL = process.env.PROTOCOL || "http";
-const API_URL = `${PROTOCOL}://${HOSTNAME}:${PORT}/api/db`;
+const API_URL = `${PROTOCOL}://${HOSTNAME}${PORT ? `:${PORT}` : ""}/api/db`;
 
 module.exports = {
   publicRuntimeConfig: {
