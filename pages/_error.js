af33dfc64e710472c51415c7c6dcd284f8ebde28
René Schubert <reneschubert@gmx.de>
2019-04-10 00:41:19 +0200

    Add error page

---
+++ pages/_error.js
@@ -0,0 +1,11 @@
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Layout from "../components/common/Layout";
+
+export default () => (
+  <Layout title="Welcome to Song Shine">
+    <Grid item xs={12}>
+      <Typography variant="h6">Not found. (Coming soon...)</Typography>
+    </Grid>
+  </Layout>
+);
